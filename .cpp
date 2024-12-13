25433e688063bb7422739a1a5e020380168ce681
Jodwinn <pavelkharkovski@gmail.com>
2024-12-13 17:08:23 +0200

    Create.cpp

---
+++ .cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <windows.h>
+#include <vector> 
+
+using namespace std;
+
+vector<int> getNegElem(int arr[], int size) {
+    vector<int> negElem; 
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0) {
+            negElem.push_back(arr[i]);
+        }
+    }
+
+    return negElem; 
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    int arr[] = { 1, -4, 7, 2, -9, 3, 0, -5 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    vector<int> negArr = getNegElem(arr, size);
+
+
+    cout << "Від'ємні елементи масиву: ";
+    for (int i = 0; i < negArr.size(); i++) {
+        cout << negArr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
